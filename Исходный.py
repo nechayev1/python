63a6459d010938d3e711b6968b63f673cf71e178
Нечаев А. И <50956637+nechayev1@users.noreply.github.com>
2020-01-13 14:20:26 +0500

    Update Исходный.py

--- Исходный.py
+++ Исходный.py
@@ -1,36 +1,35 @@
+
 # http://blog.chapagain.com.np/hash-table-implementation-in-python-data-structures-algorithms/
-# импортируем библиотеку времени, для того, чтобы засечь сколько работала программа
-import threading
-import time
 
-hash_table = [[] for _ in range(10)] 	#Создание хеш-таблицы в виде вложенного списка в 10 элементов (списки внутри списка).
+hash_table = [[] for _ in range(10)] #Создание хеш-таблицы в виде вложенного списка (списки внутри списка).
 
-# вставили данные в хэш-таблицу 
-def insert(hash_table, key, value):
-    hash_key = hash(key) % len(hash_table)
+def insert(hash_table, key, value):	#hash_table -- [[], [], [], [], [], [], [], [], [], []], key -- 10, value -- Nepal
+    hash_key = hash(key) % len(hash_table)        # hash(10) % len([[], [], [], [], [], [], [], [], [], []]), т.е hash_key == 0
     key_exists = False
-    cell = hash_table[hash_key]   	 #назовем каждый отдельный список в списке хеш-таблиц как «cell»
-    for i, kv in enumerate(cell):
+    bucket = hash_table[hash_key]    #назовем каждый отдельный список в списке хеш-таблиц как «bucket», возьмет 0-й элемент, т.е. [] -- пустой список.
+    for i, kv in enumerate(bucket):	 # выполнится bucket.append((key, value)), при (20, 'India') Цикл по enumerate выполнится и i == 0, kv == (10, 'Nepal'), k ==  10, v = Nepal
         k, v = kv
-        if key == k:                # проверяет равны ли оба операнда
-            key_exists = True       # проверка наличия ключа в словаре
-            break                   # прирывание цикла
+        if key == k:
+            key_exists = True 
+            break
     if key_exists:
-        cell[i] = ((key, value))    
+        bucket[i] = ((key, value))
     else:
-        cell.append((key, value))   # добавляет указанный элемент в конец списка
-insert(hash_table, 10, 'Nepal')     
-insert(hash_table, 25, 'USA')       # добавляет указанный элемент в список на указанную позицию
+        bucket.append((key, value))	 # append() - добавляет элемент в список
+ 
+ 
+insert(hash_table, 10, 'Nepal')		
+insert(hash_table, 25, 'USA')
 insert(hash_table, 20, 'India')
 
-# поиск данных из хэш-таблицы
-def search(hash_table, key):                    
+
+def search(hash_table, key):                    #При поиске любого ключа в хеш-таблице мы должны циклически проходить по каждому отдельному подсписку.
     hash_key = hash(key) % len(hash_table)    
-    cell = hash_table[hash_key]                         
-    for i, kv in enumerate(cell):	# цикл, enumerate() позволяет перебирать коллекцию элементов.
-        k, v = kv                   # при поиске любого ключа в хеш-таблице мы должны циклически проходить по каждому отдельному подсписку.
+    bucket = hash_table[hash_key]                         
+    for i, kv in enumerate(bucket):
+        k, v = kv
         if key == k:
-            return v                
-print (search(hash_table, 0))#  Nepal
-print (search(hash_table, 20)) #  India
-print (search(hash_table, 30)) #  None
+            return v
+print (search(hash_table, 10)) # Output: Nepal
+print (search(hash_table, 20)) # Output: India
+print (search(hash_table, 30)) # Output: None
